9dda5e706d4c8f041449ed5e9ee2128814e4fd1b
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-04-03 17:22:27 +0200

    Init project

--- technologies/db/sql/java-ee8-db-embedded-h2-helloworld-api-rest/src/main/java/com/example/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class JAXRSConfiguration extends Application {
-
-}
