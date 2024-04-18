857526799571eac6e1051532aaf82057edbc25a2
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-04-18 17:29:00 +0200

    Add asyn event

--- technologies/async/java-ee9-api-rest-helloworld-async-events-fire/src/main/java/com/example/events/items/HelloWorldEventItem.java
+++ technologies/async/java-ee9-api-rest-helloworld-async-events-fire/src/main/java/com/example/events/items/HelloWorldEventItem.java
@@ -3,13 +3,20 @@ package com.example.events.items;
 public class HelloWorldEventItem {
     
     private String message;
+    private Long nanosDelay;
 
-    public HelloWorldEventItem(String message) {
+    
+    public HelloWorldEventItem(String message, Long nanosDelay) {
         this.message = message;
+        this.nanosDelay = nanosDelay;
     }
 
     public String getMessage() {
         return message;
-    }    
+    }
+
+    public Long getNanosDelay() {
+        return nanosDelay;
+    }
 
 }
