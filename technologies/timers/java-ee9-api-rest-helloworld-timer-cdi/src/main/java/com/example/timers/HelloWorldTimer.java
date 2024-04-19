a93731efbc65a0712bd559f4877841a48a221c7e
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-04-19 08:58:47 +0200

    Update HelloWorldTimer

--- technologies/timers/java-ee9-api-rest-helloworld-timer-cdi/src/main/java/com/example/timers/HelloWorldTimer.java
+++ technologies/timers/java-ee9-api-rest-helloworld-timer-cdi/src/main/java/com/example/timers/HelloWorldTimer.java
@@ -4,15 +4,17 @@ import java.util.concurrent.TimeUnit;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.Resource;
+import jakarta.ejb.Singleton;
+import jakarta.ejb.Startup;
 import jakarta.enterprise.concurrent.ManagedScheduledExecutorService;
-import jakarta.enterprise.context.ApplicationScoped;
 
-@ApplicationScoped
+@Singleton
+@Startup
 public class HelloWorldTimer {
 
     @Resource
     private ManagedScheduledExecutorService mses;
-    
+
     @PostConstruct
     public void activateTimer() {
         mses.scheduleAtFixedRate(this::displayMessage, 0, 1, TimeUnit.SECONDS);
