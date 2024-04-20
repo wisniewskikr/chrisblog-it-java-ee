e8f13f2835d1113cad9560b184072dd1bdeded90
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-04-20 17:27:45 +0200

    Init project

---
+++ architectures/gui/jpa/java-ee9-gui-jpa-helloworld-plain-redirect/src/main/java/com/example/servlets/InitServlet.java
@@ -0,0 +1,21 @@
+package com.example.servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "InitServlet", urlPatterns = "/")
+public class InitServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        getServletContext().getRequestDispatcher("/helloworld").forward(req, resp);
+
+    }    
+    
+}
