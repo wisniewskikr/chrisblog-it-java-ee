688685214efa4214c255f9f450a36e1afc626524
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-04-20 08:25:25 +0200

    Implement Asynchronous

--- technologies/async/java-ee9-api-rest-helloworld-async-jaxrs-asynchronous/src/main/java/com/example/resources/HelloWorldResource.java
+++ technologies/async/java-ee9-api-rest-helloworld-async-jaxrs-asynchronous/src/main/java/com/example/resources/HelloWorldResource.java
@@ -4,8 +4,8 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-import jakarta.annotation.Resource;
-import jakarta.enterprise.concurrent.ManagedExecutorService;
+import jakarta.ejb.Asynchronous;
+import jakarta.ejb.Stateless;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -18,19 +18,18 @@ import jakarta.ws.rs.core.Response;
  * The REST resource implementation class.
  */
 @Path("hello")
+@Stateless
 public class HelloWorldResource {
-
-    @Resource
-    private ManagedExecutorService mes;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Asynchronous
     public void helloWorldAsync(@Suspended AsyncResponse asyncResponse) {
 
         asyncResponse.setTimeout(10, TimeUnit.SECONDS);
         asyncResponse.setTimeoutHandler(response -> response.resume(Response.status(Response.Status.SERVICE_UNAVAILABLE)));
         
-        mes.execute(() -> asyncResponse.resume(helloWorld()));
+        asyncResponse.resume(helloWorld());
 
     }
 
