21838483e19713f1fbe8a5792a835d514155799d
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-04-21 10:40:35 +0200

    Add helloworld.jsp file

--- architectures/gui/jpa/java-ee9-gui-jpa-helloworld-plain-single/src/main/java/com/example/servlets/HelloWorldServlet.java
+++ architectures/gui/jpa/java-ee9-gui-jpa-helloworld-plain-single/src/main/java/com/example/servlets/HelloWorldServlet.java
@@ -14,8 +14,8 @@ public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         
-        resp.setContentType("text/html");
-        resp.getWriter().println("<p>Hello World!</p>");
+        req.setAttribute("message", "Hello World!");
+        getServletContext().getRequestDispatcher("/helloworld.jsp").forward(req, resp);
 
     }    
     
