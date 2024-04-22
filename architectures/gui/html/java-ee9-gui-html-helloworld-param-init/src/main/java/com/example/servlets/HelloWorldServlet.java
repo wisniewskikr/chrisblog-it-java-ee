b2bace370bb7ab1999a1214d4d1c3d54fae55819
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-04-22 19:19:53 +0200

    Implement init param

--- architectures/gui/html/java-ee9-gui-html-helloworld-param-init/src/main/java/com/example/servlets/HelloWorldServlet.java
+++ architectures/gui/html/java-ee9-gui-html-helloworld-param-init/src/main/java/com/example/servlets/HelloWorldServlet.java
@@ -3,12 +3,13 @@ package com.example.servlets;
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebInitParam;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/", initParams = {@WebInitParam(name="message", value = "Hello World!")})
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
