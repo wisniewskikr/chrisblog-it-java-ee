c3daa78125412a5c55ce94949fecd4d647e66ca4
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-04-22 08:25:16 +0200

    Init project

---
+++ architectures/gui/jsp/java-ee9-gui-jsp-helloworld-form-single/src/main/java/com/example/servlets/HelloWorldServlet.java
@@ -0,0 +1,22 @@
+package com.example.servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        req.setAttribute("message", "Hello World!");
+        getServletContext().getRequestDispatcher("/helloworld.jsp").forward(req, resp);
+
+    }    
+    
+}
