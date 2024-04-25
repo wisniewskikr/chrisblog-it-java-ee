f5db60983ebb97188aa4a921eace2c5926f3417d
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-04-25 16:55:08 +0200

    Init project

---
+++ architectures/gui/jsf/java-ee9-gui-jsf-helloworld-plain-single/src/main/java/com/example/beans/HelloWorldBean.java
@@ -0,0 +1,16 @@
+package com.example.beans;
+
+import java.io.Serializable;
+
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+
+@Named("helloWorldBean")
+@ViewScoped
+public class HelloWorldBean implements Serializable {
+    
+    public String getMessage() {
+        return "Hello World!";
+    }
+
+}
