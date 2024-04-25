1fc1396e67b05f060904d904280939846f199a95
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-04-25 17:43:38 +0200

    Implement form

--- architectures/gui/jsf/java-ee9-gui-jsf-helloworld-form-single/src/main/java/com/example/beans/HelloWorldBean.java
+++ architectures/gui/jsf/java-ee9-gui-jsf-helloworld-form-single/src/main/java/com/example/beans/HelloWorldBean.java
@@ -2,15 +2,34 @@ package com.example.beans;
 
 import java.io.Serializable;
 
-import jakarta.faces.view.ViewScoped;
+import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Named;
 
 @Named("helloWorldBean")
-@ViewScoped
+@SessionScoped
 public class HelloWorldBean implements Serializable {
     
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getMessage() {
-        return "Hello World!";
+
+        String message = "";
+
+        if (name != null) {
+            message = "Hello World " + name + "!";
+            name = null;
+        }        
+
+        return message;
+        
     }
 
 }
