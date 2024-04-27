fba7e2976417b80a41d6accfacac453e619c4c03
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-04-27 08:10:41 +0200

    Implement header

--- technologies/responses/java-ee9-api-rest-helloworld-response-header/src/main/java/com/example/resources/HelloWorldResource.java
+++ technologies/responses/java-ee9-api-rest-helloworld-response-header/src/main/java/com/example/resources/HelloWorldResource.java
@@ -1,13 +1,16 @@
 package com.example.resources;
 
+import java.net.URI;
 import java.util.Collections;
 import java.util.Map;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
 
 /**
  * The REST resource implementation class.
@@ -15,11 +18,22 @@ import jakarta.ws.rs.core.Response;
 @Path("hello")
 public class HelloWorldResource {
 
+    @Context
+    private UriInfo uriInfo;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response helloWorld() {
+
         Map<String, String> response = Collections.singletonMap("message", "Hello World!");
-        return Response.ok(response).build();
+        URI uri = uriInfo.getBaseUriBuilder()
+            .path(HelloWorldResource.class)
+            .build();
+
+        return Response.ok(response)
+            .header("location", uri)
+            .build();
+
     }
     
 }
