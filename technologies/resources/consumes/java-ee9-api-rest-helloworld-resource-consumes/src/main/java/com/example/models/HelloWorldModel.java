c89e70949e62abe4a3e28142a53396f33c421539
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-04-27 09:23:01 +0200

    Implement cosumes

---
+++ technologies/resources/consumes/java-ee9-api-rest-helloworld-resource-consumes/src/main/java/com/example/models/HelloWorldModel.java
@@ -0,0 +1,15 @@
+package com.example.models;
+
+public class HelloWorldModel {
+    
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }    
+
+}
--- technologies/resources/consumes/java-ee9-api-rest-helloworld-resource-consumes/src/main/java/com/example/resources/HelloWorldResource.java
+++ technologies/resources/consumes/java-ee9-api-rest-helloworld-resource-consumes/src/main/java/com/example/resources/HelloWorldResource.java
@@ -3,7 +3,10 @@ package com.example.resources;
 import java.util.Collections;
 import java.util.Map;
 
-import jakarta.ws.rs.GET;
+import com.example.models.HelloWorldModel;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -15,10 +18,11 @@ import jakarta.ws.rs.core.Response;
 @Path("hello")
 public class HelloWorldResource {
 
-    @GET
+    @POST
     @Produces(MediaType.APPLICATION_JSON)
-    public Response helloWorld() {
-        Map<String, String> response = Collections.singletonMap("message", "Hello World!");
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response helloWorld(HelloWorldModel model) {
+        Map<String, String> response = Collections.singletonMap("message", model.getMessage());
         return Response.ok(response).build();
     }
     
